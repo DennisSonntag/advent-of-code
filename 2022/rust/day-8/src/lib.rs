fedb682734cf4add2c5fdeaeaa4704878a5f91c9
Dennis Sonntag <sonntagdennis8@gmail.com>
2023-05-09 20:25:26 -0600

    finished day 8

--- 2022/rust/day-8/src/lib.rs
+++ 2022/rust/day-8/src/lib.rs
@@ -1,59 +1,80 @@
 #![allow(dead_code)]
 
 pub fn process_part1(input: &str) -> usize {
-	let trees = input
+	let trees: Vec<Vec<u8>> = input
 		.lines()
 		.map(|line| {
 			line.chars()
 				.map(|x| x.to_digit(10).unwrap() as u8)
-				.collect::<Vec<u8>>()
+				.collect()
 		})
-		.collect::<Vec<_>>();
+		.collect();
 
-	let mut tree_visibility = trees
+	let max_length = trees.len();
+	let line_max_length = trees[0].len();
+
+	let mut visable_trees: Vec<Vec<bool>> = trees
 		.iter()
-		.map(|x| x.iter().map(|_| false).collect::<Vec<_>>())
-		.collect::<Vec<_>>();
+		.enumerate()
+		.map(|(i, tree_line)| {
+			tree_line
+				.iter()
+				.enumerate()
+				.map(|(line_idx, _)| {
+					i == 0
+						|| i == max_length - 1 || line_idx == 0
+						|| line_idx == line_max_length - 1
+				})
+				.collect()
+		})
+		.collect();
 
-    tree_visibility = tree_visibility.iter().enumerate().map(|(idx, x)| {
+	for row in 0..max_length {
+		let mut current_tree_height = 0;
+		let mut current_tree_height1 = 0;
+		let mut current_tree_height2 = 0;
+		let mut current_tree_height3 = 0;
+		for col in 0..line_max_length {
+			// left to right
+			// if col == 0 {
+			// 	current_tree_height = trees[row][col];
+			// } else if trees[row][col] > current_tree_height {
+			// 	current_tree_height = trees[row][col];
+			// 	visable_trees[row][col] = true;
+			// }
 
-    })
+			// down to up
+			// if col == max_length - 1 {
+			// 	current_tree_height1 = trees[col][row];
+			// } else if trees[col][row] > current_tree_height1 {
+			// 	current_tree_height1 = trees[col][row];
+			// 	visable_trees[col][row] = true;
+			// }
 
-	// // left to right
-	// for x in 0..trees.len() {
-	// 	let mut max_height = 0;
-	// 	for y in 0..trees[0].len() {
-	// 		if trees[x][y] > max_height {
-	// 			max_height = trees[x][y];
-	// 			tree_visibility[x][y] = true;
-	// 		}else {
-	// 			tree_visibility[x][y] = false;
-	//
-	// 		}
-	// 	}
-	// }
-	//
-	// // left to right
-	// for x in (0..trees.len()).rev() {
-	// 	let mut max_height = 0;
-	// 	for y in 0..trees[0].len() {
-	// 		if trees[x][y] > max_height {
-	// 			max_height = trees[x][y];
-	// 			tree_visibility[x][y] = true;
-	// 		}else {
-	// 			tree_visibility[x][y] = false;
-	//
-	// 		}
-	// 	}
-	// }
+			let row = max_length - 1 - row;
+			let col = line_max_length - 1 - col;
+			// right to left
+			if row == max_length - 1 {
+				current_tree_height2 = trees[row][col];
+			} else if trees[row][col] > current_tree_height2 {
+				current_tree_height2 = trees[row][col];
+				visable_trees[row][col] = true;
+			}
 
-    dbg!(&trees);
-    dbg!(&tree_visibility);
+			// up to down
+			if col == max_length - 1 {
+				current_tree_height3 = trees[col][row];
+			} else if trees[col][row] > current_tree_height3 {
+				current_tree_height3 = trees[col][row];
+				visable_trees[col][row] = true;
+			}
+		}
+	}
 
-    tree_visibility.iter().flatten().filter(|&&v| v).count()
+	visable_trees.iter().flatten().filter(|&&v| v).count()
 }
 
-pub fn process_part2(input: &str) -> u32 {
+pub fn process_part2(_input: &str) -> u32 {
 	unimplemented!()
 }
 
